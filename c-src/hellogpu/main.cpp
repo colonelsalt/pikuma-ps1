788f2a4522e10213b7831d64a436c93f2b6d90f0
Sondre <sondre@sondre.io>
2024-04-20 10:36:18 +0100

    Drawing some primitives

--- c-src/hellogpu/main.cpp
+++ c-src/hellogpu/main.cpp
@@ -9,6 +9,10 @@ constexpr u32 SCREEN_RES_X = 320;
 constexpr u32 SCREEN_RES_Y = 240;
 constexpr u32 SCREEN_CENTRE_X = SCREEN_RES_X >> 1;
 constexpr u32 SCREEN_CENTRE_Y = SCREEN_RES_Y >> 1;
+constexpr u32 SCREEN_Z = 400;
+
+constexpr u32 OT_LENGTH = 16;
+constexpr u32 PRIMBUFF_LENGTH = 2'048;
 
 struct double_buffer
 {
@@ -19,6 +23,15 @@ struct double_buffer
 
 double_buffer g_Screen;
 u16 g_CurrentBuffer; // 0 or 1 depending on which of the two draw buffers is in front
+
+u32 g_OrderingTable[2][OT_LENGTH];
+
+u8 g_PrimBuff[2][PRIMBUFF_LENGTH];
+u8* g_NextPrim;
+
+POLY_F3* g_Triangle;
+TILE* g_Tile;
+POLY_G4* g_Quad;
 
 void ScreenInit()
 {
@@ -47,7 +60,7 @@ void ScreenInit()
 	// Initialise GTE
 	InitGeom();
 	SetGeomOffset(SCREEN_CENTRE_X, SCREEN_CENTRE_Y);
-	SetGeomScreen(SCREEN_CENTRE_X);
+	SetGeomScreen(SCREEN_Z);
 
 	// Enable display
 	SetDispMask(1);
@@ -61,16 +74,77 @@ void DisplayFrame()
 	PutDispEnv(g_Screen.Disp + g_CurrentBuffer);
 	PutDrawEnv(g_Screen.Draw + g_CurrentBuffer);
 
+	// Draw all primitives in ordering table..?!
+	DrawOTag(g_OrderingTable[g_CurrentBuffer] + OT_LENGTH - 1);
+
 	g_CurrentBuffer = !g_CurrentBuffer;
+	g_NextPrim = g_PrimBuff[g_CurrentBuffer];
 }
 
 void Setup()
 {
 	ScreenInit();
+
+	g_NextPrim = g_PrimBuff[g_CurrentBuffer];
 }
 
+static u32 s_X = 82;
+static b32 s_GoingRight = true;
 void Update()
 {
+	// Clear ordering table
+	ClearOTagR(g_OrderingTable[g_CurrentBuffer], OT_LENGTH);
+
+	// Filling in ordering table with primitives
+
+	g_Tile = (TILE*)g_NextPrim;
+	setTile(g_Tile);
+	setXY0(g_Tile, s_X, 32); // top-left pos
+	setWH(g_Tile, 64, 64); // width/height
+	setRGB0(g_Tile, 0, 255, 0); // tile colour
+	addPrim(g_OrderingTable[g_CurrentBuffer], g_Tile);
+	g_NextPrim += sizeof(TILE);
+
+	if (s_GoingRight)
+	{
+		s_X++;
+		if (s_X >= SCREEN_RES_X - 64)
+		{
+			s_GoingRight = false;
+		}
+	}
+	else
+	{
+		s_X--;
+		if (s_X <= 0)
+		{
+			s_GoingRight = true;
+		}
+	}
+
+	g_Triangle = (POLY_F3*)g_NextPrim;
+	setPolyF3(g_Triangle);
+	setXY3(g_Triangle,
+		    64, 100,
+		   200, 150,
+		    50, 220); // set vertices
+	setRGB0(g_Triangle, 255, 0, 255);
+	addPrim(g_OrderingTable[g_CurrentBuffer], g_Triangle);
+	g_NextPrim += sizeof(POLY_F3);
+
+	g_Quad = (POLY_G4*)g_NextPrim;
+	setPolyG4(g_Quad);
+	setXY4(g_Quad,
+		   200, 150,
+		   319, 200,
+		   197,  82,
+		   285,  75);
+	setRGB0(g_Quad, 248, 221, 15); // yellow
+	setRGB1(g_Quad, 7, 184, 111); // cyan
+	setRGB2(g_Quad, 197, 64, 17); // green
+	setRGB3(g_Quad, 255, 255, 255); // white
+	addPrim(g_OrderingTable[g_CurrentBuffer], g_Quad);
+	g_NextPrim += sizeof(POLY_G4);
 
 }
 
